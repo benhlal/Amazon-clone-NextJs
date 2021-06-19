1cb987fd4479ccc0e9832286cfc1781a33feacef
benhlal <benhlalyounes@gmail.com>
2021-06-19 22:06:20 +0200

    [Amazon][v0] WIP Banner Carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/header/Header";
+import Banner from "../components/banner/Banner";
 
 export default function Home() {
     return (
@@ -8,6 +9,12 @@ export default function Home() {
                 <title>Amazon 2.0</title>
             </Head>
             <Header/>
+            <main className={"max-w-screen-2xl mx-aut"}>
+                {/*BANNER*/}
+                <Banner/>
+                {/*PRODUCTS*/}
+            </main>
+
         </div>
     );
 }
